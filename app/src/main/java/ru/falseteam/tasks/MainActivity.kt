17dfa39511c29f2073def2ccd6b5736f393a9a70
vladislavsumin <vladislavsumin@mail.ru>
2019-02-23 19:54:52 +0300

    Create list example

--- app/src/main/java/ru/falseteam/tasks/MainActivity.kt
+++ app/src/main/java/ru/falseteam/tasks/MainActivity.kt
@@ -2,7 +2,16 @@ package ru.falseteam.tasks
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import io.realm.Realm
+import io.realm.RealmResults
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.falseteam.tasks.realm.model.Task
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +19,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button_add.setOnClickListener { startActivity(AddTaskActivity::class.java) }
+
+        loadList()
+    }
+
+    private fun loadList() {
+        val elements = Realm.getDefaultInstance().where(Task::class.java).findAll()
+        recycler_view.adapter = Adapter(elements)
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        elements.addChangeListener { _ -> recycler_view.adapter.notifyDataSetChanged() }
+    }
+
+
+    class Adapter(val element: RealmResults<Task>) : RecyclerView.Adapter<Adapter.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return if (element.isValid) element.size else 0
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.text.text = element[position]!!.title
+        }
+
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val text: TextView = itemView.findViewById(android.R.id.text1)
+        }
     }
 }
--- app/src/main/java/ru/falseteam/tasks/realm/model/Task.kt
+++ app/src/main/java/ru/falseteam/tasks/realm/model/Task.kt
@@ -4,7 +4,7 @@ import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
 open class Task(
-        @PrimaryKey open var id: Long = 0,
+        @PrimaryKey open var id: Int = 0,
         open var title: String? = null,
         open var text: String? = null,
         open var isComplete: Boolean = false
