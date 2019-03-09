9bb7c730d7fc4a769674cb6a91b2811e11f7a61b
CobaltVO <vlavladvladik@mail.ru>
2019-03-09 20:38:09 +0300

    New menu bar button: trash

--- app/src/main/java/ru/falseteam/tasks/ui/MainActivity.kt
+++ app/src/main/java/ru/falseteam/tasks/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package ru.falseteam.tasks.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.falseteam.tasks.R
@@ -13,5 +16,15 @@ class MainActivity : AppCompatActivity() {
         button_add.setOnClickListener { AddTaskPopup(this).show() }
 
         supportFragmentManager.beginTransaction().replace(R.id.container, TaskListFragment()).commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_bar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        Toast.makeText(this, "Trash was clicked", Toast.LENGTH_SHORT).show()
+        return super.onOptionsItemSelected(item)
     }
 }
