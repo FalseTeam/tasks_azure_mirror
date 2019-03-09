386e4f522bb33c935efd4d07814e86a8db58bf1c
CobaltVO <vlavladvladik@mail.ru>
2019-03-09 19:40:53 +0300

    The new confirming dialog before deleting tasks was added

--- app/src/debug/java/ru/falseteam/tasks/ui/DebugActivity.kt
+++ app/src/debug/java/ru/falseteam/tasks/ui/DebugActivity.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.debug.activity_debug.*
-import ru.falseteam.tasks.R
 import ru.falseteam.tasks.app.App
 import ru.falseteam.tasks.database.entity.Task
 import ru.falseteam.tasks.database.repository.TaskRepository
 import javax.inject.Inject
+import androidx.appcompat.app.AlertDialog
+import ru.falseteam.tasks.R
 
 class DebugActivity : AppCompatActivity() {
 
@@ -21,18 +22,34 @@ class DebugActivity : AppCompatActivity() {
         App.dagger.inject(this)
 
         btn_delete_all.setOnClickListener {
-            taskRepository.deleteAllAsync()
-                    .observeOn(AndroidSchedulers.mainThread()).subscribe { count ->
-                        showToast("deleted $count items")
-                    }
+            val confirmingDialog = getConfirmingDialog()
+            confirmingDialog.show()
         }
 
         btn_add_10_items.setOnClickListener {
             val list = (0 until 10).map { Task(title = "task$it") }
             taskRepository.insertAsync(list)
                     .observeOn(AndroidSchedulers.mainThread()).subscribe {
-                        showToast("items added")
+                        showToast("10 tasks were successfully added")
                     }
         }
+    }
+
+    private fun getConfirmingDialog(): AlertDialog.Builder {
+        val confirmingDialog = AlertDialog.Builder(this)
+        confirmingDialog.setTitle("Are you sure you want to erase all data?")
+                .setMessage("Make a choice, Neo!")
+                .setPositiveButton("Delete")
+                { dialog, _ ->
+                    taskRepository.deleteAllAsync()
+                            .observeOn(AndroidSchedulers.mainThread()).subscribe { count ->
+                                showToast("$count tasks were successfully deleted")
+                            }
+                    dialog.cancel()
+                }
+                .setNegativeButton("Cancel")
+                { dialog, _ -> dialog.cancel() }
+                .setCancelable(false)
+        return confirmingDialog
     }
 }
